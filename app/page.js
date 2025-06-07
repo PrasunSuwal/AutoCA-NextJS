db79eb54a4bca0eb202ec9eca653a10cf825754e
Prasun Suwal <prasun.suwal@gmail.com>
2025-06-07 23:57:12 +0545

    herosection

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/hero";
 
 export default function Home() {
-  return <Button variant="destructive">Hello World</Button>;
+  return (
+    <div className="mt-40">
+      <HeroSection />
+    </div>
+  );
 }
